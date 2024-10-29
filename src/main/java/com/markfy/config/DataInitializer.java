f2ca1cde067b45c0e6c2fc46cc7f6c03602df60f
PBPedro <pedropaulobarretta@gmail.com>
2024-10-29 19:54:02 -0300

    internacionalização

--- src/main/java/com/markfy/config/DataInitializer.java
+++ src/main/java/com/markfy/config/DataInitializer.java
@@ -26,7 +26,8 @@ public class DataInitializer {
 
     @PostConstruct
     public void init() {
-        if (usuarioRepository.findByEmailUsuario("admin@admin.com") == null) {
+        if (usuarioRepository.findByEmailUsuario("admin@admin.com") == null
+                && usuarioRepository.findByEmailUsuario("user@email.com") == null) {
             Loja loja = new Loja("Loja exemplo", "12.015.104/0001-59");
             Loja lojaSave = lojaRepository.save(loja);
 
@@ -38,6 +39,15 @@ public class DataInitializer {
             admin.setRoles(new HashSet<>(Collections.singletonList("ROLE_ADMIN")));
             admin.setLoja(lojaSave);
             usuarioRepository.save(admin);
+
+            Usuario user = new Usuario();
+            user.setNomeUsuario("User");
+            user.setSobrenomeUsuario("Unnamed");
+            user.setEmailUsuario("user@email.com");
+            user.setSenha(passwordEncoder.encode("user123"));
+            user.setRoles(new HashSet<>(Collections.singletonList("ROLE_USER")));
+            user.setLoja(lojaSave);
+            usuarioRepository.save(user);
         }
     }
 }
